81d30345de00ca907d2b6e91a1cf7558bf499889
dolby527 <dolby527@naver.com>
2025-11-28 16:10:56 +0900

    fix js sytle for multifly

--- calculator.js
+++ calculator.js
@@ -1,6 +1,15 @@
+// (x:number, y:number) => number
 function add(a,b) {
     return a+b;
 }
+
+// (X:number, Y:number) => number
 function substract(a,b){
     return a-b;
+}
+
+//string, number, null, undefined
+// (x:number, y:number) => number)
+function multiply(a,b){
+    return a*b;
 }
